e0dde62f9990741105e2b45cd840e33cc548aa60
BEJOSCHR <benjamin@schranni.de>
2022-03-30 14:39:03 +0200

    Added ConsoleInput and Output

--- OrderOfElements/src/main/java/de/bejoschgaming/orderofelements/main/OOE_Main_Server.java
+++ OrderOfElements/src/main/java/de/bejoschgaming/orderofelements/main/OOE_Main_Server.java
@@ -1,12 +1,42 @@
 package de.bejoschgaming.orderofelements.main;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import de.bejoschgaming.orderofelements.debug.ConsoleHandler;
+
 public class OOE_Main_Server {
 
+	private static long startMillis = 0;
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Starting Order of Elements - OOE");
+		ConsoleHandler.printMessageInConsole("Starting OrderOfElements_Server [OOE_S]...", true);
 		
+		ConsoleHandler.startUserInputScanner();
+		
+		startMillis = System.currentTimeMillis();
+		ConsoleHandler.printMessageInConsole("Startup finished!", true);
 		
 	}
 
+	public static void terminateProgramm() {
+		
+		ConsoleHandler.printMessageInConsole("Stopping OrderOfElements_Server [OOE_S]...", true);
+		
+		ConsoleHandler.stopUserInputScanner();
+		
+		ConsoleHandler.printMessageInConsole("Shuttdown finished! [Runtime: "+(System.currentTimeMillis()-startMillis)/1000/60+" Minutes]", true);
+		
+		//CLOSE DLEAY
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				ConsoleHandler.printMessageInConsole("Terminating...", true);
+				System.exit(0);
+			}
+		}, 1000*3);
+		
+	}
+	
 }
